b121b4b7fa30d3a288ff1ba56eb3666b84382e63
heavencat1 <2504604719@qq.com>
2022-07-16 23:21:11 +0800

    这是一次新的提交

---
+++ javaproject/src/SecondJava.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+import  java.math.*;
+
+public class SecondJava {
+
+    public static void main(String[] args) {
+//        for(int i=1;i<=5; i++){
+//            System.out.println((int)(Math.random()*100));
+//        }
+//
+//        int i = 0;
+//        while(i<=5){
+//            System.out.println("helloword");
+//            if(i ==3){
+//                break;
+//            }
+//            i++;
+//          }
+
+//        int num = 0;
+//        for(int i=1; i<=100; i++){
+//            num += i;
+//            if(num>20){
+//                break;
+//            }
+//           }
+//        System.out.println(num);
+
+        Scanner input = new Scanner(System.in);
+//        String rightUser = "居健";
+//        String rightPassword = "123";
+//        for(int i=1; i<=3; i++){
+//            System.out.print("请输入用户名:");
+//            String user = input.next();
+//
+//
+//            System.out.print("请输入密码:");
+//            String password = input.next();
+//
+//            if(user.equals(rightUser) && password.equals(rightPassword)){
+//                System.out.print("欢迎登录");
+//                break;
+//            }else{
+//                System.out.println("用户名错误,还有"+(3-i)+"次机会");
+//            }
+//        }
+
+
+
+//        double money = 100000;
+//        int num = 0;
+//        while(true){
+//            if(money>50000){
+//                money *= 0.95 ;
+//                num += 1;
+//                continue;
+//            }else if(money<=50000 && money>1000){
+//                money -= 1000;
+//                num += 1;
+//                continue;
+//            }else if(money<1000){
+//                break;
+//            }
+//        }
+//        System.out.println(num);
+
+//
+//        System.out.print("输入一个整数:");
+//        int num = input.nextInt();
+//
+//        if(num>0){
+//            System.out.println("大于0");
+//        }else if(num<0){
+//            System.out.println("小于0");
+//        }else{
+//            System.out.println("等于0");
+//        }
+//
+//
+        System.out.print("输入一个整数:");
+        int num = input.nextInt();
+
+//        int num = 0;
+//        for(int i = 1;i<=100;i++){
+//            if (i%5 != 0){
+//
+//                System.out.print(i+"\t");
+//                num +=1;
+//
+//                if(num%5 ==0){
+//                    System.out.print("\n");
+//                }
+//            }
+//        }
+
+//        int firstNum = num/100;
+//        int secondNum =num%100/10;
+//        int thirdNum = num%10;
+//        int num2 = firstNum*firstNum*firstNum+secondNum*secondNum*secondNum+thirdNum*thirdNum*thirdNum;
+//        if (num ==num2){
+//            System.out.println("是水仙花数");
+//        }else{
+//            System.out.println("不是水仙花数");
+//        }
+
+
+
+    }
+
+
+
+
+
+}
--- javaproject/src/helloword.java
+++ javaproject/src/helloword.java
@@ -8,28 +8,123 @@ import java.util.concurrent.CancellationException;
 
 
 public class helloword {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("请输入名字:");
-        String name = input.next();
-
-        System.out.print("请输入年龄:");
-        int age = input.nextInt();
-
-        System.out.print("请输入工资:");
-        double score = input.nextDouble();
-
-        System.out.println("名字是:"+name+",年龄是:"+age+",工资是:"+score);
+//        int count = 10;
+//        for(int i=0; i<=count; i++){
+//
+//                System.out.println(i+"+"+(count-i)+"="+count);
+//
+//
+//        }
 
 
 
+//        int num = 1;
+//        while(num<=100){
+//            if(num%3 == 0){
+//                System.out.println(num);
+//            }
+//            num += 1;
+//        }
+
+//          int num = 40;
+//          int num1 = 200;
+//          int num2 = 2;
+//          while(num<=num1){
+//              if(num % num2 == 0){
+//                  System.out.println(num);
+//              }
+//              num += 1;
+//          }
+
+//        int i = 1;
+//        int num = 0;
+//        do{
+//
+//            num += i;
+//
+//            i++;
+//        }while(i<=100);
+//        System.out.println(num);
+//        System.out.println(i);
+
+
+//
+//        int i = 1;
+//        int num = 0;
+//        do{
+//            if(i%5==0 && i%3!=0){
+//                num += 1;
+//            }
+//            i++;
+//        }while(i <= 200);
+//        System.out.println(num);
+
+
+//         char answer = ' ';
+//         do{
+//             System.out.println("还钱吗");
+//             answer = input.next().charAt(0);
+//
+//         }while(answer != 'y');
+
+
+//        double score = 0.0;
+//        double zongfen = 0.0;
+//        double allscore = 0.0;
+//        int num = 0;
+//        int i = 1;
+//        for(; i<=3;i++){
+//            for(int j=1;j<=5;j++){
+//                System.out.print("第"+i+"个班的第"+j+"个学生的成绩是：");
+//                score = input.nextDouble();
+//                zongfen += score;
+//                allscore += zongfen;
+//                if(j == 5){
+//                    double pingjunfen = zongfen/5;
+//                    System.out.println("第"+i+"个班的平均分是:"+pingjunfen);
+//
+//                    zongfen = 0;
+//                }
+//
+//                if(score>=60){
+//                    num +=1;
+//                }
+//            }
+//
+//        }
+//        System.out.println(i-1+"个班及格的人总共有:"+num+"人");
+//        System.out.println("总分是:"+allscore);
+//        System.out.println("总平均分是:"+allscore/(i-1));
 
 
 
+        for(int i=1;i<=5;i++){
+            for(int x=1;x<=5-i;x++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=2*i-1;j++){
+                if(i == 5){
+                    System.out.print("*");
+                }else{
+                    if(j ==1 || j==2*i-1){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+            }
+
+            System.out.print("\n");
+        }
 
 
 
     }
+
 }
+
 
